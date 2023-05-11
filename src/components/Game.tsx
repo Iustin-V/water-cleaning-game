dd05f3facdf8042379f6b534ba3f8c17ea2e891a
Iustin Vasiliu <iustinv2.0@gmail.com>
2023-05-11 16:37:00 +0300

    code improvement

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,6 +5,7 @@ import tire from "../images/tire.png";
 import plastic from "../images/plastic.png";
 import metal from "../images/metal.png";
 import sticla from "../images/sticla.png";
+import heart from "../images/heart.png";
 
 const GameWrapper = styled.div`
   display: flex;
@@ -79,27 +80,93 @@ const Control = styled.div`
     height: 100%;
   }
 `;
-const Image = styled.img<{ position: number }>`
+const Image = styled.img<{ position: number; animationDuration?: number }>`
   width: 50px;
   position: absolute;
+  color: red;
+  animation-duration: ${(props) => props.animationDuration}!important;
   top: 100%;
   visibility: hidden;
   left: ${(props) => props.position}%;
 `;
+
+const LivesContainer = styled.div`
+  top: 15px;
+  right: 15px;
+  display: flex;
+  position: absolute;
+  flex-direction: row;
+  gap: 15px;
+`;
+const ScoreContainer = styled.div`
+  top: 15px;
+  left: 15px;
+  display: flex;
+  position: absolute;
+  flex-direction: row;
+  gap: 15px;
+`;
+const Heart = styled.img`
+  width: 20px;
+`;
 export const Game = () => {
+  //  refs
   const containerRef = React.useRef<HTMLDivElement | null>(null);
   const buttonOneRef = React.useRef<HTMLDivElement | null>(null);
   const buttonTwoRef = React.useRef<HTMLDivElement | null>(null);
   const buttonThreeRef = React.useRef<HTMLDivElement | null>(null);
   const trashRef = React.useRef<HTMLImageElement | null>(null);
-  const [gameStarted, setGameStarted] = React.useState(false);
 
+  // essentials
+  const [gameStarted, setGameStarted] = React.useState(false);
+  const [nextLevelScore, setNextLevelScore] = useState(500);
+  const [imgPosition, setImgPosition] = React.useState(
+    Math.floor(Math.random() * 80)
+  );
+
+  //  current data
+  const [currentLevel, setCurrentLevel] = useState(0);
   const [currentImage, setCurrentImage] = useState({ id: 0, url: "" });
   const [currentBasket, setCurrentBasket] = useState({ id: 0, url: "" });
-  let imageUrls = [can, tire, can, tire];
+
+  //  stats
+  const [lives, setLives] = useState(3);
+  const [score, setScore] = useState(0);
+
+  // let imageUrls = [can, tire, can, tire];
+  let imageUrls = [tire, tire, tire, tire];
+
+  // game logic
+  const handleGameReset = () => {
+    setGameStarted(false);
+    setScore(0);
+    setLives(3);
+    setCurrentImage({ id: 0, url: "" });
+  };
+
+  const handleBasket = (id: number) => {
+    setCurrentBasket({ id: id, url: "" });
+    console.log("currentbasket", currentBasket.id);
+
+    buttonOneRef.current?.classList.remove("active");
+    buttonTwoRef.current?.classList.remove("active");
+    buttonThreeRef.current?.classList.remove("active");
+    switch (id) {
+      case 0:
+        buttonOneRef.current?.classList.add("active");
+        break;
+      case 1:
+        buttonTwoRef.current?.classList.add("active");
+        break;
+      case 2:
+        buttonThreeRef.current?.classList.add("active");
+        break;
+    }
+  };
 
   React.useEffect(() => {
     if (gameStarted) {
+      setImgPosition(Math.floor(Math.random() * 80));
       setCurrentImage({
         id: currentImage.id + 1,
         url: imageUrls[Math.floor(Math.random() * 3)],
@@ -109,47 +176,65 @@ export const Game = () => {
 
   React.useEffect(() => {
     if (gameStarted) {
-      setTimeout(() => {
-        if (
-          trashRef.current?.getBoundingClientRect().y &&
-          trashRef.current?.getBoundingClientRect().y > 600
-        ) {
-          setCurrentImage({
-            id: currentImage.id + 1,
-            url: imageUrls[Math.floor(Math.random() * 3)],
-          });
+      if (lives === 0) {
+        alert(`Game Over!
+        Your score is : ${score}, congratulations!`);
+        handleGameReset();
+      } else if (score === nextLevelScore) {
+        const userConfirmed = window.confirm(
+          `Congratulations, you finished the current level. Proceed to the next one?`
+        );
+        if (userConfirmed) {
+          setCurrentLevel(currentLevel + 1);
+          setNextLevelScore(nextLevelScore + 200);
+          handleGameReset();
         }
-      }, 2000);
+      } else {
+        setTimeout(() => {
+          if (
+            trashRef.current?.getBoundingClientRect().y &&
+            trashRef.current?.getBoundingClientRect().y > 550
+          ) {
+            console.log(currentBasket.id, currentImage.url);
+            if (
+              (currentBasket.id === 0 && currentImage.url === tire) ||
+              (currentBasket.id === 1 && currentImage.url === can)
+            ) {
+              setScore(score + 100);
+            } else {
+              setLives(lives - 1);
+            }
+            setImgPosition(Math.floor(Math.random() * 80));
+            setCurrentImage({
+              id: currentImage.id + 1,
+              url: imageUrls[Math.floor(Math.random() * 3)],
+            });
+          }
+        }, 2000);
+      }
     }
   }, [currentImage, gameStarted]);
 
   React.useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "a" || event.key === "A") {
-        // setCurrentBasket({ id: 1, url: "" });
-        console.log("aaaaaaa");
-        buttonOneRef.current?.classList.add("active");
-        buttonTwoRef.current?.classList.remove("active");
-        buttonThreeRef.current?.classList.remove("active");
+        handleBasket(0);
       }
       if (event.key === "s" || event.key === "S") {
-        // setCurrentBasket({ id: 1, url: "" });
-        console.log("aaaaaaa");
-        buttonOneRef.current?.classList.remove("active");
-        buttonTwoRef.current?.classList.add("active");
-        buttonThreeRef.current?.classList.remove("active");
+        handleBasket(1);
       }
       if (event.key === "d" || event.key === "D") {
-        // setCurrentBasket({ id: 1, url: "" });
-        console.log("aaaaaaa");
-        buttonOneRef.current?.classList.remove("active");
-        buttonTwoRef.current?.classList.remove("active");
-        buttonThreeRef.current?.classList.add("active");
+        handleBasket(2);
       }
     };
 
     window.addEventListener("keydown", handleKeyDown);
   }, []);
+
+  /* nu merge schimbarea duratiei animatiei, 
+  nu merge selectarea cosului pentru runda curenta, 
+  schimbarea are loc abia runda urm*/
+
   return (
     <GameWrapper>
       <LegendContainer></LegendContainer>
@@ -166,35 +251,44 @@ export const Game = () => {
             )
           }
         >
+          <ScoreContainer>
+            {score} / {nextLevelScore}
+          </ScoreContainer>
+          <LivesContainer>
+            {Array.from({ length: lives }).map((_, i) => (
+              <Heart src={heart} alt="heart" />
+            ))}
+          </LivesContainer>
           {!gameStarted && (
             <StartButton
               onClick={() => {
                 setGameStarted(true);
               }}
             >
-              START
+              START Level {currentLevel}
             </StartButton>
           )}
           {
             <Image
-              position={Math.floor(Math.random() * 80)}
+              position={imgPosition}
               src={currentImage.url}
               ref={trashRef}
               alt=""
               key={currentImage.id}
+              animationDuration={2 - currentLevel * 0.1}
               className={`slide-down`}
             />
           }
         </GameViewContainer>
         <ButtonContainer>
-          <Control ref={buttonOneRef}>
-            <img src={plastic} />
+          <Control ref={buttonOneRef} onClick={() => handleBasket(0)}>
+            <img src={plastic} onClick={() => handleBasket(0)} />
           </Control>
-          <Control ref={buttonTwoRef}>
-            <img src={metal} />
+          <Control ref={buttonTwoRef} onClick={() => handleBasket(1)}>
+            <img src={metal} onClick={() => handleBasket(1)} />
           </Control>
-          <Control ref={buttonThreeRef}>
-            <img src={sticla} />
+          <Control ref={buttonThreeRef} onClick={() => handleBasket(2)}>
+            <img src={sticla} onClick={() => handleBasket(2)} />
           </Control>
         </ButtonContainer>
       </MainContainer>
